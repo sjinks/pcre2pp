82a6a2a317ddec7e61ff0ef9d8bea81a271450fc
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-08-08 08:14:53 +0300

    Refactor .travis.yml

---
+++ .travis/before_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-get update -qq
+sudo apt-get install libgtest-dev g++-6
+git clone --depth=1 --branch=ci-packages https://github.com/sjinks/pcre2pp.git packages
+sudo apt-get purge libpcre3-dev
+sudo dpkg -i packages/*.deb
+git checkout master
